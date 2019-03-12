31d5e49d4e0dd6977c2ccbe991baa246a7acd312
Akshay Gupta <akshay.gupta@entytle.com>
2019-03-12 23:14:22 +0530

    Css for Beer details modal

--- src/containers/DetailModal/DetailModal.js
+++ src/containers/DetailModal/DetailModal.js
@@ -4,6 +4,8 @@ import classnames from 'classnames';
 
 import {hideBeerDetailsModal} from '../../redux/actions';
 
+import './detailModal.scss';
+
 class DetailModal extends Component {
     render(){
         const {show, data} = this.props;
@@ -16,30 +18,41 @@ class DetailModal extends Component {
                 className='beer-details-modal'
             >
                 <Modal.Header>
-                    <div onClick={e => this.onHideHandler(e)}>x</div>
+                    <span className='icon icon-cross' onClick={e => this.onHideHandler(e)}></span>
                 </Modal.Header>
                 <Modal.Body>
                     <div className={classnames('beer-details-content', 'row')}>
                         <div className={classnames('beer-details-image', 'col-sm-3')}>
                             <img src={data.image_url} />
                         </div>
-                        <div className={classnames('beer-details-image', 'col-sm-9')}>
-                            <div className={classnames('beer-details-image')}>
+                        <div className={classnames('beer-details-text', 'col-sm-9')}>
+                            <div className={classnames('beer-details-name')}>
                                 {data.name}
                             </div>
-                            <div className={classnames('beer-details-image')}>
+                            <div className={classnames('beer-details-tagline')}>
                                 {data.tagline}
                             </div>
-                            <div className={classnames('beer-details-image')}>
-                                {data.ibu}
-                                {data.abv}
-                                {data.ebc}
+                            <div className='dark-line' />
+                            <div className={classnames('beer-details-extras')}>
+                                <div className='extras-section'>
+                                    <span className='extras-name'>IBU:</span>
+                                    <span className='extras-value'>{data.ibu}</span>
+                                </div>
+                                <div className='extras-section'>
+                                    <span className='extras-name'>ABV:</span>
+                                    <span className='extras-value'>{data.abv}</span>
+                                </div>
+                                <div className='extras-section'>
+                                    <span className='extras-name'>EBC:</span>
+                                    <span className='extras-value'>{data.ebc}</span>
+                                </div>
                             </div>
-                            <div className={classnames('beer-details-image')}>
+                            <div className={classnames('beer-details-desc')}>
                                 {data.description}
                             </div>
-                            <div className={classnames('beer-details-image')}>
-                                {this.renderBestServedWith(food_pairing)}
+                            <div className={classnames('beer-details-pairings')}>
+                                <div className='pairings-text'>Best served with:</div>
+                                <ul>{this.renderBestServedWith(food_pairing)}</ul>
                             </div>
                         </div>
                     </div>
@@ -53,7 +66,7 @@ class DetailModal extends Component {
         if(Array.isArray(arr)){
             const length = arr.length;
             const servedWith = arr.map((item, index) => {
-                return <div key={index}>{item}</div>;
+                return <li key={index} className='pairings-item'>{item}</li>;
             })
             console.log(servedWith);
             return servedWith;
