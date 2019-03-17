fa82474d12fd730fed1d6ab37a5048351defa8a3
Akshay Gupta <akshay.gupta@entytle.com>
2019-03-18 01:30:09 +0530

    Updates

---
+++ precache-manifest.3f0ece8c9e377c544a1ada5ba4fdf177.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b48839b53828c177b20884ffa1f72556",
+    "url": "/Bear-With-Me/static/media/icomoon.b48839b5.svg"
+  },
+  {
+    "revision": "717b21124a0bad33aca136cc0a24be95",
+    "url": "/Bear-With-Me/static/media/icomoon.717b2112.ttf"
+  },
+  {
+    "revision": "0b728d4d850d48a9ad19802ccc6e13b6",
+    "url": "/Bear-With-Me/static/media/icomoon.0b728d4d.woff"
+  },
+  {
+    "revision": "02724e9cd809e80b518124cead55dd14",
+    "url": "/Bear-With-Me/static/media/icomoon.02724e9c.eot"
+  },
+  {
+    "revision": "b10d4c155805482988b8",
+    "url": "/Bear-With-Me/static/js/runtime~main.b10d4c15.js"
+  },
+  {
+    "revision": "15816e92496e934d8c92",
+    "url": "/Bear-With-Me/static/js/main.15816e92.chunk.js"
+  },
+  {
+    "revision": "8b26464805ab388d2897",
+    "url": "/Bear-With-Me/static/js/2.8b264648.chunk.js"
+  },
+  {
+    "revision": "15816e92496e934d8c92",
+    "url": "/Bear-With-Me/static/css/main.c59090cd.chunk.css"
+  },
+  {
+    "revision": "8b26464805ab388d2897",
+    "url": "/Bear-With-Me/static/css/2.51941159.chunk.css"
+  },
+  {
+    "revision": "52a529ee2531822cd620d318d583dcf5",
+    "url": "/Bear-With-Me/index.html"
+  }
+];
